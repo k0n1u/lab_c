0317fa507d82d76633e3b22e3caa1c69557f8e2b
Tomasz Stępkowski <tomek.k0n1u@gmail.com>
2012-11-18 10:39:54 +0100

    updateAll

--- cw18-11-12/ex2.c
+++ cw18-11-12/ex2.c
@@ -1,12 +1,25 @@
+//zad2 - drukowanie szachownicy
 #include <stdio.h>
+
 
 int main()
 {
-  int i;
+  int i,j;
   
   for(i=1;i<9;i++)
     {
-      printf("* ");
+      for(j=1;j<9;j++)
+	{
+	  if(i%2!=0)
+	    {
+	      printf("* ");
+	    }
+	  else
+	    {
+	      printf(" *");
+	    }
+	}
+      printf("\n");
     }
 
   return 0;
