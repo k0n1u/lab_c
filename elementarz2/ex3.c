8c125895e7aa350793d42dff7325281f4d7deceb
k0n1u <tomek.k0n1u@gmail.com>
2012-12-06 12:20:32 +0100

    poprawki

--- elementarz2/ex3.c
+++ elementarz2/ex3.c
@@ -8,12 +8,9 @@ int main()
   double pi=4;
   double wynik[4];
   double j=3,k=1;
-  // const stala = 4;
 
-
-  for(int i=1;i<=10000;i++)
+  for(int i=1;i<=1000000;i++)
     {
-      // double temp=i;
       pi+=(pow(-1,i) * (4/j));
       j+=2;
 
@@ -27,18 +24,18 @@ int main()
       case 1000:
 	wynik[2]=pi;
 	break;
-      case 10000:
+      case 1000000:
 	wynik[3]=pi;
 	break;
       }
 
-      system("clear");
-      printf("Trwają oblicznia: %.2lf",((k/10000)*100));
+      //system("clear");
+      printf("Trwają oblicznia: %.2lf",((k/1000000)*100));
       putchar('%');
-      putchar('\n');
+      putchar('\r');
       k++;
     }
-
-  printf("sumowano razy|\t10|\t100|\t1000|\t10000|\n");
-  printf("wynik|\t%lf|\t%lf|\t%lf|\t%lf|\n",wynik[0],wynik[1],wynik[2],wynik[3]);
+  putchar('\n');
+  printf("sumowano razy|   10   |  100   |  1000  | 1000000|\n");
+  printf("wynik        |%.6lf|%.6lf|%.6lf|%.6lf|\n",wynik[0],wynik[1],wynik[2],wynik[3]);
 }
