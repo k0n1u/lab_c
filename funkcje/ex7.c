2c60e28f8079252429d3dcba6b1e70929a28a905
Tomasz Stępkowski <tomek.k0n1u@gmail.com>
2012-12-09 09:45:31 +0100

    jeszcze nieskończone

--- funkcje/ex7.c
+++ funkcje/ex7.c
@@ -32,35 +32,60 @@ void komentator(int wynik)
 {
   int rndVar;
   rndVar=rnd(1,3);
-
+  
   switch (rndVar) {
-  case 1: if(wynik==1)
-      printf
+  case 1: 
+    if(wynik==1) {
+      printf("Bardzo dobrze!\n");
+    }
+    else {
+      printf("Zła odpowiedź.\n");
+    }
+    break;
+
+  case 2:
+    if (wynik == 1) {
+      printf("Świetnie!\n");
+    }
+    else {
+      printf("Oj, niedobrze.\n");
+    }
+    break;
+  case 3:
+    if (wynik == 1){
+      printf("Dobra robota!\n");
+    }
+    else {
+      printf("Następnym razem pójdzie Ci lepiej.\n");
+    }
+    break;
+  }
 }
 
   
 
 void mainMenu()
 {
-  int exitCounter=0,wynik,a,b,badAnswerCount=0,goodAnswerCount=0;
+  int exitCounter=0,wynik,a,b,badAnswerCount=0,goodAnswerCount=0,answerValue;
   printf("Program spradzi Twoją umiejętność mnożenia.\n");
   
   while(exitCounter!=5)
     {
       printf("\n");
-      a=rnd(1,100);
-      b=rnd(1,100);
+      a=rnd(1,10);
+      b=rnd(1,10);
       printf("Ile to jest %d * %d ?\n",a,b);
       scanf("%d",&wynik);
+      answerValue=checker(a,b,wynik);
 
-      if(checker(a,b,wynik)==1)
+      if(answerValue == 1)
 	{
-	  printf("Dobrze!\n");
+	  komentator(answerValue);
 	  goodAnswerCount++;
 	}
       else
 	{
-	  printf("Źle!\n");
+	  komentator(answerValue);
 	  badAnswerCount++;
 	}
 	 
