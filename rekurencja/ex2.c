bd874cd2777497d991482eb2e854856edbcf5571
k0n1u <tomek.k0n1u@gmail.com>
2012-12-16 10:12:00 +0100

    newton

---
+++ rekurencja/ex2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int newton(int n,int k)
+{
+	if(k==0 || k==n)
+		return 1;
+	if(k>0 && k<n)
+		return newton(n-1,k-1)+newton(n-1,k);
+}
+
+
+
+int main()
+{
+	
+	printf("%d\n",newton(15,8));
+
+
+
+}
